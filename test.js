b79243e2aba03646119af4fd4a47301f0448f759
Marcin Wrochna <m.wrochna@mimuw.edu.pl>
2019-10-20 19:27:48 +0100

    Added tests.

--- test.js
+++ test.js
@@ -12,15 +12,33 @@ const ltwa = fs.readFileSync('./LTWA_20170914-modified.csv', 'utf8');
 const shortWords = fs.readFileSync('./shortwords.txt', 'utf8');
 const abbrevIso = new AbbrevIso.AbbrevIso(ltwa, shortWords);
 
-let t = process.argv.slice(2).join(' ');
-t = t.normalize('NFC');
-console.log('Abbreviation using all language rules:');
-console.log(abbrevIso.makeAbbreviation(t));
-console.log('Abbreviation using only english rules:');
-console.log(abbrevIso.makeAbbreviation(t, ['eng', 'mul', 'lat', 'und']));
-console.log('Matching patterns:');
-const matchingPatterns = abbrevIso.getMatchingPatterns(t);
-for (const pattern of matchingPatterns) {
-    console.log(pattern.line);
+let t = process.argv.slice(2).join(' ').trim();
+if (t.length) {
+    t = t.normalize('NFC');
+    console.log('Abbreviation using all language rules:');
+    console.log(abbrevIso.makeAbbreviation(t));
+    console.log('Matching patterns:');
+    const matchingPatterns = abbrevIso.getMatchingPatterns(t);
+    for (const pattern of matchingPatterns)
+        console.log(pattern.line);
+    console.log('Possible compound patterns:');
+    const compoundPatterns = abbrevIso.getMatchingPatterns(t, undefined, true);
+    for (const pattern of compoundPatterns)
+        if (!matchingPatterns.includes(pattern))
+            console.log(pattern.line);
+} else {
+    let rl = require('readline').createInterface({
+        input: fs.createReadStream('./tests.csv', { encoding: 'utf8' })
+    });
+    rl.on('line', (line) => {
+        line = line.split('\t', 3);
+        const abbrev = abbrevIso.makeAbbreviation(line[0]);
+        if (abbrev !== line[1] && line[1] !== '???') {
+            console.log(`Mismatch:\t${line[0]}\nIs:       \t${abbrev}\nShould be:\t${line[1]}`);
+            if (line[2])
+                console.log(line[2]);
+            console.log('');
+        }
+    });
 }
 
